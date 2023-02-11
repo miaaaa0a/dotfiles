377b776b0e9d4568e3410a2032097a6e7ac92ea5
redoverflow <punctuwuation@gmail.com>
2023-02-11 14:10:32 +0300

    major overhaul of polybar config

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+# colors
+export BACKGROUND_COL="#11111b"
+export MODULE_BG_COL="#1e1e2e"
+export MAUVE_COL="#cba6f7"
+export GREEN_COL="#a6e3a1"
+
+# suffix for rounded modules
+export ROUNDED_PREFIX="%{T2}%{F${MODULE_BG_COL}}%{B${BACKGROUND_COL}}"
+
 # Terminate already running bar instances
 # If all your bars have ipc enabled, you can use 
 polybar-msg cmd quit
